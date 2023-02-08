7417c5faff2c4ac80010ac75652d4aca5d89c22c
antheboets <anthe.boets@student.ehb.be>
2023-02-08 17:40:37 +0100

    adding json fetch of dates

--- js/main.js
+++ js/main.js
@@ -1,17 +1,51 @@
 // let dates = [ { "date": Date.parse("8 February 2023 2:30am"), "length": 3}, 
 //           { "date": Date.parse("11 February 2023 14:30"), "length": 6} ]
 
-
+/*
 let dates = [
   { "date": Date.parse("8 February 2023 2:30 am GMT"), "length": 3 },
   { "date": Date.parse("8 February 2023 7:30 pm GMT"), "length": 3 },
   { "date": Date.parse("11 February 2023 1:30 pm GMT"), "length": 6 }
 ]
-
+*/
+let dates = []
 let countdownDate = 0;
 let statusPage = "";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+  //fetching the dates data
+  //dates = await fetch("./data/dates.json").json()
+  dates = await fetch("./data/dates.json").then((req)=>{return req.json()})
+  /*  
+  dates = [
+      {
+          "unixTimestamp":1675823400000,
+          "dateText":"8 February 2023 2:30 am GMT",
+          "playtestDuration":10800000,
+          "playtestDurationText":"3 hours"
+      }, 
+      {
+          "unixTimestamp":1675884600000,
+          "dateText":"8 February 2023 7:30 pm GMT",
+          "playtestDuration":10800000,
+          "playtestDurationText":"3 hours"
+      }, 
+      {
+          "unixTimestamp":1676122200000,
+          "dateText":"11 February 2023 1:30 pm GMT",
+          "playtestDuration":18000000,
+          "playtestDurationText":"6 hours"
+      }
+  ]
+  */
+  console.log(dates)
+
+  //sort list on oldest to newst, just to be sure
+  dates.sort((a,b) =>{
+    return a - b
+  })
+  console.log(dates)
+
   toggleTheme()
 
   display()
@@ -40,18 +74,18 @@ function display(){
 function calcDates() {
   for (const date of dates) {
     console.log("date: ", date)
-    console.log("date diff: ", date["date"] - Date.parse(new Date()))
+    console.log("date diff: ", date.unixTimestamp - Date.parse(new Date()))
 
-    if (date["date"] - Date.parse(new Date()) > 0) {
+    if (date.unixTimestamp - Date.parse(new Date()) > 0) {
       // still in the future
       console.log("still in the future")
-      countdownDate = date["date"] / 1000;
+      countdownDate = date.unixTimestamp / 1000;
       statusPage = "countdown"
       break;
 
     } else {
       // already happened
-      let endDate = date["date"] + (date["length"] * 60 * 60 * 1000)
+      let endDate = date.unixTimestamp + date.playtestDuration
       console.log("endDate: ", endDate)
 
       if (endDate - Date.parse(new Date()) > 0) {
